08df23529c3c66025a5fdf03b327afcfdb38cf67
yonathan3801911625 <yonathan.3801911625@ucaldas.edu.co>
2022-04-05 18:34:53 -0500

    first commit

---
+++ src/test/java/edu/ucaldas/gps/GpsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucaldas.gps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
